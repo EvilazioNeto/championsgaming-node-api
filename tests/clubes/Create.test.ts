a5dac68aa077cadec26ff9b701c6e74c89c56f88
evilazioneto <evilazioneto943@gmail.com>
2024-09-03 16:36:41 -0300

    fix club test

--- tests/clubes/Create.test.ts
+++ tests/clubes/Create.test.ts
@@ -2,96 +2,87 @@ import { StatusCodes } from "http-status-codes";
 import { testServer } from "../jest.setup"
 
 describe('Clube - Create', () => {
-    it('Criar clube com sucesso', async () => {
+    let accessToken = '';
+    let usuarioId: number;
+
+    const criarUsuarioEObterToken = async () => {
         const email = 'teste@gmail.com';
-        const usuario = await testServer.post('/cadastrar').send({
+        const usuarioRes = await testServer.post('/cadastrar').send({
             nome: 'Teste',
             email,
             senha: '12345678'
-        })
+        });
+        
+        const signInRes = await testServer.post('/entrar').send({ email, senha: '12345678' });
+        accessToken = signInRes.body.accessToken;
+        usuarioId = usuarioRes.body; 
+    };
 
-        const signInRes = await testServer.post('/entrar').send({ email, senha: '12345678' })
+    beforeAll(async () => {
+        await criarUsuarioEObterToken();
+    });
 
+    it('Criar clube com sucesso', async () => {
         const res = await testServer
             .post('/clubes')
-            .set({ Authorization: `Bearer ${signInRes.body.accessToken}` })
+            .set({ Authorization: `Bearer ${accessToken}` })
             .send({
                 nome: "Benfica",
                 mascote: "Águia",
                 cor_principal: "Vermelho",
                 cor_secundaria: "branco",
-                usuarioId: usuario.body
-            })
+                usuarioId,
+                fotoUrl: "https://firebasestorage.googleapis.com/v0/b/reactuploads-92499.appspot.com/o/users%2F1%2Ffiles%2FCampeonato_Brasileiro_de_Futebol_de_2022_-_S%C3%A9rie_A.png?alt=media&token=8e06489a-8a63-4499-803d-f5c6cbb2634b"
+            });
 
-        expect(res.statusCode).toEqual(StatusCodes.CREATED)
-        expect(typeof res.body).toEqual('number');
-    })
+        expect(res.statusCode).toEqual(StatusCodes.CREATED);
+        expect(typeof res.body).toEqual('number'); 
+    });
 
     it('Tenta criar um clube com um atributo que não é permitido', async () => {
-        const email = 'teste@gmail.com';
-        const usuario = await testServer.post('/cadastrar').send({
-            nome: 'Teste',
-            email,
-            senha: '12345678'
-        })
-
-        const signInRes = await testServer.post('/entrar').send({ email, senha: '12345678' })
-
         const res = await testServer
             .post('/clubes')
-            .set({ Authorization: `Bearer ${signInRes.body.accessToken}` })
+            .set({ Authorization: `Bearer ${accessToken}` })
             .send({
                 nome: "Benfica",
                 mascote: "Águia",
                 cor_principal: "Vermelho",
                 cor_secundaria: "branco",
-                usuarioId: usuario.body,
-                treinador: "JJ"
-            })
+                usuarioId,
+                fotoUrl: "https://firebasestorage.googleapis.com/v0/b/reactuploads-92499.appspot.com/o/users%2F1%2Ffiles%2FCampeonato_Brasileiro_de_Futebol_de_2022_-_S%C3%A9rie_A.png?alt=media&token=8e06489a-8a63-4499-803d-f5c6cbb2634b",
+                treinador: "JJ" 
+            });
 
-        expect(res.statusCode).toEqual(StatusCodes.BAD_REQUEST)
-    })
+        expect(res.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    });
 
     it('Tenta criar um clube sem o token de autenticação', async () => {
-        const email = 'teste@gmail.com';
-        await testServer.post('/cadastrar').send({
-            nome: 'Teste',
-            email,
-            senha: '12345678'
-        })
-
         const res = await testServer
             .post('/clubes')
             .send({
                 nome: "Benfica",
                 mascote: "Águia",
                 cor_principal: "Vermelho",
-                cor_secundaria: "branco"
-            })
+                cor_secundaria: "branco", 
+                usuarioId,
+                fotoUrl: "https://firebasestorage.googleapis.com/v0/b/reactuploads-92499.appspot.com/o/users%2F1%2Ffiles%2FCampeonato_Brasileiro_de_Futebol_de_2022_-_S%C3%A9rie_A.png?alt=media&token=8e06489a-8a63-4499-803d-f5c6cbb2634b",
+            });
 
-        expect(res.statusCode).toEqual(StatusCodes.UNAUTHORIZED)
-    })
+        expect(res.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
+    });
 
     it('Tenta criar um clube com dados faltando', async () => {
-        const email = 'teste@gmail.com';
-        const usuario = await testServer.post('/cadastrar').send({
-            nome: 'Teste',
-            email,
-            senha: '12345678'
-        })
-
-        const signInRes = await testServer.post('/entrar').send({ email, senha: '12345678' })
-
         const res = await testServer
             .post('/clubes')
-            .set({ Authorization: `Bearer ${signInRes.body.accessToken}` })
+            .set({ Authorization: `Bearer ${accessToken}` })
             .send({
                 mascote: "Águia",
                 cor_principal: "Vermelho",
                 cor_secundaria: "branco",
-                usuarioId: usuario.body
-            })
+                usuarioId
+                
+            });
 
-        expect(res.statusCode).toEqual(StatusCodes.BAD_REQUEST)
-    })
-})
+        expect(res.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    });
+});
